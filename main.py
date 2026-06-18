9e7ade3b225a284b3a60d863921b0e34848a0270
Vladislav Ishchenko <vlad.ishch3nko@gmail.com>
2026-06-19 00:29:03 +0300

    Delete /hi

--- main.py
+++ main.py
@@ -36,16 +36,6 @@ async def cmd_help(message: types.Message):
         text = "**Команды ученика:**\n/slots - свободные уроки\n/book - записаться"
     await message.answer(text, parse_mode="Markdown")
 
-
-
-@dp.message(Command("hi"))
-async def cmd_help(message: types.Message):
-    
-    text = "Приветствую))"
-    await message.answer(text, parse_mode="Markdown")
-
-
-
 @dp.message()
 async def unknown(message: types.Message):
     await message.answer("Я не знаю такой команды.")
